419593b3de2344882c9985a9f9b1acdbc52ae3f8
System Administrator <root@CRHSJ105814.local>
2019-03-05 14:34:04 -0500

    Nag first commit

---
+++ clin-search-engine-core/src/main/java/org/chursj/search/poc/TextNlpCommand.java
@@ -0,0 +1,30 @@
+package org.chursj.search.poc;
+
+public class TextNlpCommand {
+
+	/**
+	 * This method will take initial content and apply the openNlp on that content
+	 * 
+	 * @param content at input
+	 * @return formated ready to index content.
+	 * 
+	 *         // in this serial exec, I : 
+	 *         // - Catch the content. // - Apply
+	 *         openNlp ( - propositions, duplicate words, summarization)
+	 * 
+	 */
+	public static byte[] applyNlp(String content) {
+
+		byte[] formattedContent = null;
+
+		
+		
+		
+		
+		
+		
+		
+		return formattedContent;
+	}
+
+}
