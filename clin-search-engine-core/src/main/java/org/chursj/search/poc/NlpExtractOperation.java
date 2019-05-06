96240d97f031920ac5a51374ba5ffcc564fd0e3c
Velythyl <charlie.gauthier@umontreal.com>
2019-05-06 16:17:45 -0400

    start of refactor

--- clin-search-engine-core/src/main/java/org/chursj/search/poc/NlpExtractOperation.java
+++ clin-search-engine-core/src/main/java/org/chursj/search/poc/NlpExtractOperation.java
@@ -22,95 +22,43 @@ import rita.RiTa;
 
 public class NlpExtractOperation {
 
-	/**
-	 * This method will take initial content and apply the openNlp on that content
-	 * 
-	 * @param content at input
-	 * @return formated ready to index content.
-	 * 
-	 *         // in this serial exec, I : // - Catch the content. // - Apply
-	 *         openNlp ( - propositions, duplicate words, summarization)
-	 * @throws IOException
-	 * 
-	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static List<ExtractContentBean> applyNlp(String pageContent, String Filename, String[] excludeWords,
-			String[] posList, int extractLevel, String mode) throws IOException {
+    /**
+     * This method will take initial content and apply the openNlp on that content
+     *
+     * @param content at input
+     * @return formated ready to index content.
+     * <p>
+     * // in this serial exec, I : // - Catch the content. // - Apply
+     * openNlp ( - propositions, duplicate words, summarization)
+     * @throws IOException
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static List<ExtractContentBean> applyNlp(String pageContent, String Filename, String[] excludeWords,
+                                                    String[] posList, int extractLevel, String mode) throws IOException {
 
-		// adding config for text analysis.
-		final Map ritaConfig = new HashMap();
-		ritaConfig.put("wordCount", 1);
-		ritaConfig.put("ignoreCase", true);
-		ritaConfig.put("ignoreStopWords", true);
-		ritaConfig.put("ignorePunctuation", true);
-		ritaConfig.put("wordsToIgnore", excludeWords);
-		
-		String[] tokenContent = RiTa.tokenize(pageContent);
-		int len = tokenContent.length;
-		List<ExtractContentBean> words = new ArrayList();
-		for (int i = 0; i < len; ++i) {
-			String word = tokenContent[i];
-			final ExtractContentBean contentBo = new ExtractContentBean();
-			contentBo.setWord(word);
-			RiString myRiString = new RiString(tokenContent[i]);
-			String pos = myRiString.pos()[0];
-			contentBo.setPartOfSpeech(pos);
-			contentBo.setFile(Filename);
-			words.add(contentBo);
-		}
-		
-		return words;
-		/*Map<String, Integer> nlpTextMap = RiTa.concordance(pageContent, ritaConfig);
-		if (mode.equals("word")) {
-			try {
-				Map<String, String> pageMap = new HashMap<String, String>();
-				nlpTextMap.keySet().stream().forEach(nlpElmt -> {
-					RiString myRiString = new RiString(nlpElmt);
-					String pos = myRiString.pos()[0];
-					int count = nlpTextMap.get(nlpElmt);
-					String key = nlpElmt;
-					String value = String.valueOf(count) + "_" + pos;
-					boolean contains = Arrays.stream(posList).anyMatch(pos::equals);
-					if ((contains == true) && (count >= extractLevel)) {
-						pageMap.put(key, value);
-					} else if ((posList[0].length() == 0) && (count >= extractLevel)) {
-						pageMap.put(key, value);
-					}
-				});
-				return pageMap;
-			} catch (Exception e) {
-				return null;
-			}
-		} else {
-			try {
-				// Map<String, Integer> nlpTextMap = RiTa.kwic(text, word)(pageContent,
-				// ritaConfig);
+        // adding config for text analysis.
+        final Map ritaConfig = new HashMap();
+        ritaConfig.put("wordCount", 1);
+        ritaConfig.put("ignoreCase", true);
+        ritaConfig.put("ignoreStopWords", true);
+        ritaConfig.put("ignorePunctuation", true);
+        ritaConfig.put("wordsToIgnore", excludeWords);
 
-				Map<String, String> pageMap = new HashMap<String, String>();
-				nlpTextMap.keySet().stream().forEach(nlpElmt -> {
-					RiString myRiString = new RiString(nlpElmt);
-					String pos = myRiString.pos()[0];
-					int count = nlpTextMap.get(nlpElmt);
-					String key = nlpElmt;
-					String[] value = RiTa.kwic(pageContent, key);
-					int len = value.length;
-					boolean contains = Arrays.stream(posList).anyMatch(pos::equals);
-					if ((contains == true) && (count >= extractLevel)) {
-						for (int x = 0; x < len; ++x) {
-							pageMap.put(key, value[x]);
-						}
-					} else if ((posList[0].length() == 0) && (count >= extractLevel)) {
-						for (int x = 0; x < len; ++x) {
-							pageMap.put(key, value[x]);
-						}
-					}
-				});
-				return pageMap;
-			} catch (Exception e) {
-				return null;
-			}
-		}*/
+        String[] tokenContent = RiTa.tokenize(pageContent);
 
-	}
+        List<ExtractContentBean> words = new ArrayList();
+        for (String word : tokenContent) {
+            final ExtractContentBean contentBo = new ExtractContentBean();
+            contentBo.setWord(word);
+            RiString myRiString = new RiString(word);
+            String pos = myRiString.pos()[0];
+            contentBo.setPartOfSpeech(pos);
+            contentBo.setFile(Filename);
+            words.add(contentBo);
+        }
+
+        return words;
+
+    }
 
 }
--- clin-search-engine-core/src/main/java/org/chursj/search/poc/PdfContentIndexer.java
+++ clin-search-engine-core/src/main/java/org/chursj/search/poc/PdfContentIndexer.java
@@ -21,65 +21,68 @@ import io.searchbox.indices.CreateIndex;
 
 public class PdfContentIndexer {
 
-	@SuppressWarnings({ "unchecked", "unused" })
-	public boolean doIndex(ExtractContentBean content, String serverUrl) throws IOException {
-		JestClientFactory factory = new JestClientFactory();
-        factory.setHttpClientConfig(new HttpClientConfig.Builder("http://142.1.177.88:8082")
-                .defaultCredentials("admin", "admin")
+    @SuppressWarnings({"unchecked", "unused"})
+    public boolean doIndex(ExtractContentBean content, String serverUrl) throws IOException {
+        //https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html
+
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(new HttpClientConfig.Builder(serverUrl)
+                //.defaultCredentials("admin", "admin")
                 .multiThreaded(true)
-                    .build());
+                .build());
         JestClient client = factory.getObject();
         Settings.Builder settingsBuilder = Settings.builder();
         settingsBuilder.put("number_of_shards", 5);
         settingsBuilder.put("number_of_replicas", 1);
         client.execute(new CreateIndex.Builder("searchcontent").settings(settingsBuilder.build().getAsStructuredMap()).build());
-       
+
         Index index = new Index.Builder(content).index("searchcontent").type("pdfdata").build();
         DocumentResult result = client.execute(index);
         System.out.println("CREATED ===> " + result.isSucceeded());
         return result.isSucceeded();
 
-	}
-	public boolean indexer(ExtractContentBean content, String serverUrl) {
-		try {
-			// build Es client factory.
-			Map<String, Object> settings = new HashMap<>();
-			settings.put("number_of_shards", 8);
+    }
 
-			// String mappingsJson = "{\"type1\":
-			// {\"_source\":{\"enabled\":false},\"properties\":{\"field1\":{\"type\":\"keyword\"}}}}";
+    public boolean indexer(ExtractContentBean content, String serverUrl) {
+        try {
+            // build Es client factory.
+            Map<String, Object> settings = new HashMap<>();
+            settings.put("number_of_shards", 8);
 
-			final JestClientFactory factory = new JestClientFactory();
+            // String mappingsJson = "{\"type1\":
+            // {\"_source\":{\"enabled\":false},\"properties\":{\"field1\":{\"type\":\"keyword\"}}}}";
 
-			HttpClientConfig httpCfg = new HttpClientConfig.Builder(serverUrl).multiThreaded(true)
-					.defaultMaxTotalConnectionPerRoute(4).defaultCredentials("admin", "admin").maxTotalConnection(20)
-					.build();
-			factory.setHttpClientConfig(httpCfg);
+            final JestClientFactory factory = new JestClientFactory();
 
-			final JestClient client = factory.getObject();
-			final CreateIndex createIndex = new CreateIndex.Builder("searchcontent").settings(settings)
-					// .mappings(mappingsJson)
-					.payload((Map<String, Object>) new ObjectMapper().convertValue(content,
-							new TypeReference<Map<String, Object>>() {
-							}))
-					.build();
+            HttpClientConfig httpCfg = new HttpClientConfig.Builder(serverUrl).multiThreaded(true)
+                    .defaultMaxTotalConnectionPerRoute(4).defaultCredentials("admin", "admin").maxTotalConnection(20)
+                    .build();
+            factory.setHttpClientConfig(httpCfg);
 
-			final Index createIndexo = new Index.Builder(content)
+            final JestClient client = factory.getObject();
+            final CreateIndex createIndex = new CreateIndex.Builder("searchcontent").settings(settings)
+                    // .mappings(mappingsJson)
+                    .payload((Map<String, Object>) new ObjectMapper().convertValue(content,
+                            new TypeReference<Map<String, Object>>() {
+                            }))
+                    .build();
+
+            final Index createIndexo = new Index.Builder(content)
                     .index("searchcontent")
                     //.type("searchcontent")
-                   // .id(content.getFile())
+                    // .id(content.getFile())
                     .setHeader("Method", HttpMethod.PUT)
                     .build();
-			
-			JestResult result;
-			result = client.execute(createIndexo);
-			return result.isSucceeded();
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
+            JestResult result;
+            result = client.execute(createIndexo);
+            return result.isSucceeded();
 
-	}
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 }
